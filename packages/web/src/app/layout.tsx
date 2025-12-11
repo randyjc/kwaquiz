7feb579d948b2715781882e9a02c297ff3aa69f6
RandyJC <rchompff@gmail.com>
2025-12-11 11:14:56 +0100

    1

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -16,7 +16,7 @@ const montserrat = Montserrat({
 export async function generateMetadata(): Promise<Metadata> {
   const theme = getTheme()
   return {
-    title: theme.brandName || "Quiz",
+    title: theme.brandName || undefined,
     icons: "/icon.svg",
   }
 }
